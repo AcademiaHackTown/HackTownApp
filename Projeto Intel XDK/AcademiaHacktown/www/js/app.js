b067a56cb8538cdc4b3a3a0aee69c740959a3e14
Allysson Fernando Guedes de Almeida <allyssonif@gmail.com>
2017-04-25 06:45:17 -0300

    Segunda versão

--- Projeto Intel XDK/AcademiaHacktown/www/js/app.js
+++ Projeto Intel XDK/AcademiaHacktown/www/js/app.js
@@ -1,36 +1,101 @@
-var servidor = "http://www.valeapp.com.br/app";
+var           servidor = 'http://hacktown.petrolina.ifsertao-pe.edu.br/_mobile/requests/',
+                 login = "login.php",
+        ranking_grupos = "ranking_grupos.php",
+    ranking_individual = "ranking_individual.php";
 
-function onAppReady() {
-    if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
-        navigator.splashscreen.hide() ;
+    function load(){
+      $.mobile.loading( "show", {
+            theme: $.mobile.loader.prototype.options.theme,
+      });
     }
-}
-document.addEventListener("app.Ready", onAppReady, false);
-$("document").ready(function(){
-	$("#menulojas").on("click", function(){
-		$.mobile.changePage( "catalogo.html", { transition: "slide", changeHash: false });
-		getLojas();
-	});
-});
-function getLojas(){
-			$.ajax({
-					url: servidor+"/web/getLojas.php",
-					cache: false,
-					beforeSend: function(){
-					},
-					complete: function(){
-					},
-					success: function(categorias){
-						$("#catalogo").empty();
-						if(categorias.retorno != false){
-							$.each(categorias, function(x, arq){						
-								//$("#catalogo").append("<div class='col-xs-6 col-sm-3'><div class='box'><a href='#' class='ui-btn ui-btn-inline ui-btn-icon-block icones' OnClick='getSubcategorias(\" "+arq.id+"\",\""+arq.nome+"\")'><img src='"+servidor+"/assets/files/icons/"+arq.img+"' class='img img-lista'><br>"+arq.nome+"</div></div>");
-								$(".listaEvento").append("<li data-icon='false' style='border-bottom: 1px solid lightgray'><a href='#'><img src='"+servidor+"/assets/files/empresas/"+arq.img+"' class='img img-responsive'><h3>"+arq.nome+"</h3></a></li>");
-								$(".listaEvento").listview("refresh");
-							});
-						}else{
-							alert("Houve um erro na Página. Tente novamente");
-						}
-					}	
-			});
-  };
+    function hide(){
+      $.mobile.loading( "hide" );
+    }
+    $(document).on("pageinit", function(){
+      $("[id$=individualRanking]").click(function(){
+        $.mobile.changePage( "ranking_individual.html", { transition: "fade", changeHash: false });
+        getIndividualRanking();
+      });
+      $("[id$=groupRanking]").click(function(){
+        $.mobile.changePage( "ranking_grupo.html", { transition: "fade", changeHash: false });
+        getGroupRanking();
+      });
+      $("[id$=home]").click(function(){
+        $.mobile.changePage( "index.html", { transition: "fade", changeHash: false });
+      });
+    });
+
+function getIndividualRanking(){
+  $.ajax({
+    url: servidor+ranking_individual,
+    cache: false,
+    beforeSend: function(){
+    },
+    complete: function(){
+    },
+    success: function(result){
+      $("#tabelaRanking").empty();
+      $.each(result, function(i, turma){
+         var i = 0;
+         var j= 0;
+         $.each(turma, function(c, cadaturma){
+              i++;
+              if(i%2 == 0){
+                var tabela = "<div data-role='collapsible' data-collapsed-icon='carat-d' data-expanded-icon='carat-u' class='collapse-ranking' id='collapse' data-inset='false'><h3>"+cadaturma.nome+"</h3><ul data-role='listview' class='listaRank'>";
+              }else{
+                var tabela = "<div data-role='collapsible' data-collapsed-icon='carat-d' data-expanded-icon='carat-u' class='collapse-ranking-imp' id='collapse' data-inset='false'><h3>"+cadaturma.nome+"</h3><ul data-role='listview' class='listaRank'>";
+              }
+              //console.log(cadaturma.nome);
+              $.each(cadaturma.usuarios, function(x, aluno){
+                j++;
+                if(j%2 == 0){
+                  tabela += "<li><div class='ui-grid-a'><div class='ui-block-a aluno'>"+aluno.nome+"</div> <div class='ui-block-b pts'>"+aluno.pontos+" pontos</div></li>";
+                }else{
+                  tabela += "<li><div class='ui-grid-a'><div class='ui-block-a aluno-imp'>"+aluno.nome+"</div> <div class='ui-block-b pts-imp'>"+aluno.pontos+" pontos</div></li>";
+                }
+              });
+              tabela +="</ul></div>";
+              $("#tabelaRanking").append(tabela);
+              $("#tabelaRanking").trigger("create");
+          });
+      });
+    }
+  });
+};
+function getGroupRanking(){
+  $.ajax({
+    url: servidor+ranking_grupos,
+    cache: false,
+    beforeSend: function(){
+    },
+    complete: function(){
+    },
+    success: function(result){
+      $("#tabelaGRanking").empty();
+      $.each(result, function(i, turma){
+         var i = 0;
+         var j= 0;
+         $.each(turma, function(c, cadaturma){
+              i++;
+              if(i%2 == 0){
+                var tabela = "<div data-role='collapsible' data-collapsed-icon='carat-d' data-expanded-icon='carat-u' class='collapse-ranking'  id='collapse' data-inset='false'><h3>"+cadaturma.nome+"</h3><ul data-role='listview' class='listaRank'>";
+              }else{
+                var tabela = "<div data-role='collapsible' data-collapsed-icon='carat-d' data-expanded-icon='carat-u' class='collapse-ranking-imp'  id='collapse' data-inset='false'><h3>"+cadaturma.nome+"</h3><ul data-role='listview' class='listaRank'>";
+              }
+              //console.log(cadaturma.nome);
+              $.each(cadaturma.grupos, function(x, grupo){
+                j++;
+                if(j%2 == 0){
+                  tabela += "<li><div class='ui-grid-a'><div class='ui-block-a aluno'>"+grupo.nome+"</div> <div class='ui-block-b pts'>"+grupo.pontos+" pontos</div></li>";
+                }else{
+                  tabela += "<li><div class='ui-grid-a'><div class='ui-block-a aluno-imp'>"+grupo.nome+"</div> <div class='ui-block-b pts-imp'>"+grupo.pontos+" pontos</div></li>";
+                }
+              });
+              tabela +="</ul></div>";
+              $("#tabelaGRanking").append(tabela);
+              $("#tabelaGRanking").trigger("create");
+          });
+      });
+    }
+  });
+};
